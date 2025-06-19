29f47310dbe949f5fe4a563363dbe96d5b46d935
20145659 <20145659@tafe.wa.edu.au>
2025-06-19 21:24:50 +0800

    Added config save/load methods to CarPark

--- src/car_park.py
+++ src/car_park.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 from datetime import datetime
 from sensor import Sensor
@@ -13,6 +14,14 @@ class CarPark:
 
         self.log_file = log_file if isinstance(log_file, Path) else Path(log_file)
         self.log_file.touch(exist_ok=True)  # creates the log file if it doesn't exist
+
+    def write_config(self):
+        with open("config.json", "w") as f:
+            json.dump({
+                "location": self.location,
+                "capacity": self.capacity,
+                "log_file": str(self.log_file)
+            }, f)
 
     def _log_car_activity(self, plate, action):
         with self.log_file.open("a") as f:
@@ -55,3 +64,10 @@ class CarPark:
         }
         for display in self.displays:
             display.update(data)
+
+    @classmethod
+    def from_config(cls, config_file=Path("config.json")):
+        config_file = config_file if isinstance(config_file, Path) else Path(config_file)
+        with config_file.open() as f:
+            config = json.load(f)
+        return cls(config["location"], config["capacity"], log_file=config["log_file"])
